d557e6b5519da9a868881784ec6ddb868b95c3b0
BusinessJoe <lukasboelling002@gmail.com>
2023-12-01 01:28:06 -0500

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,135 @@
+use std::io::{self, BufRead};
+use regex::Regex;
+
+use anyhow::Result;
+
+fn parse_line(s: &str) -> u32 {
+    let first_char = s.chars().find(|c| c.is_digit(10)).unwrap();
+    let last_char = s.chars().rev().find(|c| c.is_digit(10)).unwrap();
+
+    first_char.to_digit(10).unwrap() * 10 + last_char.to_digit(10).unwrap()
+}
+
+fn parse_num(num: &str) -> u32 {
+    match num {
+        "1" | "one" => 1,
+        "2" | "two" => 2,
+        "3" | "three" => 3,
+        "4" | "four" => 4,
+        "5" | "five" => 5,
+        "6" | "six" => 6,
+        "7" | "seven" => 7,
+        "8" | "eight" => 8,
+        "9" | "nine" => 9,
+        _ => unreachable!()
+    }
+}
+
+fn parse_wordy_line(s: &str) -> u32 {
+    let first_re: Regex = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine|[1-9]).*").unwrap();
+    let last_re: Regex = Regex::new(r".*(one|two|three|four|five|six|seven|eight|nine|[1-9])").unwrap();
+    let first_match = first_re.captures(s).unwrap().get(1).unwrap();
+    let last_match = last_re.captures(s).unwrap().get(1).unwrap();
+
+    parse_num(first_match.as_str()) * 10 + parse_num(last_match.as_str())
+}
+
+fn find_calibration_sum(lines: &[String]) -> u32 {
+    lines.iter().map(|l| parse_line(l)).sum()
+}
+
+fn find_wordy_calibration_sum(lines: &[String]) -> u32 {
+    lines.iter().map(|l| parse_wordy_line(l)).sum()
+}
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+    let lines: Vec<String> = stdin.lock().lines().map(|r| r.unwrap()).collect();
+
+    let sum = find_calibration_sum(&lines);
+    let wordy_sum = find_wordy_calibration_sum(&lines);
+    println!("Sum: {}", sum);
+    println!("Wordy Sum: {}", wordy_sum);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{find_calibration_sum, find_wordy_calibration_sum, parse_wordy_line};
+
+    #[test]
+    fn test_example() {
+        let lines: Vec<String> = vec![
+            "1abc2".to_string(),
+            "pqr3stu8vwx".to_string(),
+            "a1b2c3d4e5f".to_string(),
+            "treb7uchet".to_string(),
+        ];
+
+        let sum = find_calibration_sum(&lines);
+        assert_eq!(142, sum);
+    }
+
+    #[test]
+    fn test_wordy() {
+        let lines: Vec<String> = vec![
+            "two1nine".to_string(),
+            "eightwothree".to_string(),
+            "abcone2threexyz".to_string(),
+            "xtwone3four".to_string(),
+            "4nineeightseven2".to_string(),
+            "zoneight234".to_string(),
+            "7pqrstsixteen".to_string(),
+        ];
+        let sum = find_wordy_calibration_sum(&lines);
+        assert_eq!(281, sum);
+    }
+
+    #[test]
+    fn test_simple() {
+        let val = parse_wordy_line("two1");
+        assert_eq!(21, val);
+    }
+
+    #[test]
+    fn test_zero() {
+        let val = parse_wordy_line("120");
+        assert_eq!(12, val);
+    }
+
+    #[test]
+    fn test_one_num() {
+        let val = parse_wordy_line("seven");
+        assert_eq!(77, val);
+    }
+
+    #[test]
+    fn test_surrounding_garbage() {
+        let val = parse_wordy_line("zzzsevenxx13xxnineyy");
+        assert_eq!(79, val);
+    }
+
+    #[test]
+    fn test_doubles() {
+        let cases = [
+            ("oneone", 11),
+            ("twotwo", 22),
+            ("threethree", 33),
+            ("fourfour", 44),
+            ("fivefive", 55),
+            ("sixsix", 66),
+            ("sevenseven", 77),
+            ("eighteight", 88),
+            ("ninenine", 99),
+        ];
+        for case in cases.iter() {
+            assert_eq!(case.1, parse_wordy_line(case.0));
+        }
+    }
+
+    #[test]
+    fn test_overlap() {
+        assert_eq!(18, parse_wordy_line("oneight"));
+    }
+}
