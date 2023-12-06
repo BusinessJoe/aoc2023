99b80033b560d9a46d64fa78feba513189a0200b
BusinessJoe <lukasboelling002@gmail.com>
2023-12-05 21:46:37 -0500

    Optimize by mapping intervals

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,9 @@
-use std::{cmp::Ordering, collections::{BTreeMap, BTreeSet}, array, io};
+use std::cmp;
 use std::io::BufRead;
+use std::{
+    cmp::Ordering,
+    io,
+};
 
 #[derive(Debug, PartialOrd, PartialEq, Eq)]
 struct MapEntry {
@@ -9,7 +13,9 @@ struct MapEntry {
 }
 impl MapEntry {
     pub fn parse(line: &str) -> Self {
-        let mut nums = line.split_ascii_whitespace().map(|s| s.parse::<usize>().unwrap());
+        let mut nums = line
+            .split_ascii_whitespace()
+            .map(|s| s.parse::<usize>().unwrap());
         let dest = nums.next().unwrap();
         let start = nums.next().unwrap();
         let len = nums.next().unwrap();
@@ -30,42 +36,101 @@ impl MapEntry {
     }
 }
 
+struct Map {
+    entries: Vec<MapEntry>
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            entries: vec![]
+        }
+    }
+
+    pub fn add(&mut self, entry: MapEntry) {
+        self.entries.push(entry);
+        self.entries.sort();
+    }
+
+    fn map_id(&self, id: usize) -> usize {
+        for e in self.entries.iter() {
+            if let Some(id) = e.map(id) {
+                return id;
+            }
+        }
+        id
+    }
+
+    pub fn map_interval(&self, interval: (usize, usize)) -> Vec<(usize, usize)> {
+        let mut out = vec![];
+        let mut remaining = interval;
+        let mut start = interval.0;
+        let mut end = 0;
+
+        self.entries.iter().skip_while(|entry| {
+            entry.end < interval.0
+        }).take_while(|entry| {
+            interval.1 >= entry.start
+        }).for_each(|entry| {
+            debug_assert_ne!(remaining.0, remaining.1);
+
+            start = cmp::max(remaining.0, entry.start);
+            if start > remaining.0 {
+                // handle remainder
+                out.push((remaining.0, start));
+            }
+
+            end = cmp::min(remaining.1, entry.end);
+            remaining.0 = end;
+            out.push((start + entry.dest - entry.start, end + entry.dest - entry.start));
+        });
+        if remaining.0 != remaining.1 {
+            out.push(remaining);
+        }
+        
+        if out.is_empty() {
+            vec![interval]
+        } else {
+            out.sort();
+            out
+        }
+    }
+}
+
+
 impl Ord for MapEntry {
     fn cmp(&self, other: &Self) -> Ordering {
-        if self.start >= other.end {
-            Ordering::Greater
-        } else if self.end <= other.start {
-            Ordering::Less
-        } else {
-            self.start.cmp(&other.start)
-        }
+        self.start.cmp(&other.start)
     }
 }
 
 fn get_seeds(seeds_line: &str) -> Vec<usize> {
     let no_header = seeds_line.split_once(':').unwrap().1;
-    no_header.split_whitespace().map(|s| s.parse::<usize>().unwrap()).collect()
+    no_header
+        .split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect()
 }
 
-fn generate_maps(lines: &[String]) -> Vec<Vec<MapEntry>> {
+fn generate_maps(lines: &[String]) -> Vec<Map> {
     let mut lines = lines.iter();
 
     // Pop empty line.
     lines.next();
 
-    let mut maps: Vec<Vec<MapEntry>> = Vec::new();
+    let mut maps: Vec<Map> = Vec::new();
 
     lines.next(); // Consume title
-    maps.push(Vec::new());
+    maps.push(Map::new());
     while let Some(line) = lines.next() {
         if line.is_empty() {
             lines.next(); // Consume title
-            maps.push(Vec::new());
+            maps.push(Map::new());
             continue;
         }
-        let map = MapEntry::parse(line);
+        let map_entry = MapEntry::parse(line);
         let idx = maps.len() - 1;
-        maps[idx].push(map); 
+        maps[idx].add(map_entry);
     }
 
     maps
@@ -73,41 +138,40 @@ fn generate_maps(lines: &[String]) -> Vec<Vec<MapEntry>> {
 
 fn solution1(lines: &[String]) -> usize {
     let seeds_line = &lines[0];
-    let seeds = get_seeds(&seeds_line);
+    let seeds = get_seeds(seeds_line);
 
     let maps = generate_maps(&lines[1..]);
 
-    seeds.into_iter().map(|s| {
-        maps.iter().fold(s, |s, entries| {
-            for e in entries {
-                if let Some(s) = e.map(s) {
-                    return s;
-                }
-            }
-            return s;
+    seeds
+        .into_iter()
+        .map(|s| {
+            maps.iter().fold(s, |s, map| {
+                map.map_id(s)
+            })
         })
-    }).min().unwrap()
+        .min()
+        .unwrap()
 }
+
 
 fn solution2(lines: &[String]) -> usize {
     let seeds_line = &lines[0];
-    let seeds = get_seeds(&seeds_line);
+    let seeds = get_seeds(seeds_line);
 
     let maps = generate_maps(&lines[1..]);
 
-    seeds.chunks_exact(2).map(|chunk| {
-        let range = chunk[0]..chunk[0] + chunk[1];
-        range.into_iter().map(|s| {
-            maps.iter().fold(s, |s, entries| {
-                for e in entries {
-                    if let Some(s) = e.map(s) {
-                        return s;
-                    }
-                }
-                return s;
-            })
-        }).min().unwrap()
-    }).min().unwrap()
+    seeds
+        .chunks_exact(2)
+        .map(|chunk| {
+            let interval = (chunk[0], chunk[0] + chunk[1]);
+            let mut intervals = maps.iter().fold(vec![interval], |intervals, map| {
+                intervals.into_iter().flat_map(|interval| map.map_interval(interval)).collect()
+            });
+            intervals.sort();
+            intervals[0].0
+        })
+        .min()
+        .unwrap()
 }
 
 fn main() {
@@ -118,4 +182,24 @@ fn main() {
     let p2 = solution2(&lines);
     println!("Part 1: {p1}");
     println!("Part 2: {p2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_map_interval() {
+        let mut map = Map::new();
+        map.add(MapEntry {
+            start: 10,
+            end: 20,
+            dest: 50,
+        });
+
+        let intervals = map.map_interval((0, 100));
+        let expected: Vec<(usize, usize)> = vec![(0, 10), (20, 100), (50, 60)];
+
+        assert_eq!(expected, intervals);
+    }
 }
