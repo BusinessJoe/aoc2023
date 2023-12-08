3c76c0a02b9133345051081d476707e3da98fbdc
BusinessJoe <lukasboelling002@gmail.com>
2023-12-08 01:50:05 -0500

    Improve performance

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
-use std::{io, cmp};
 use std::io::BufRead;
+use std::{cmp, io};
 
 type Map = HashMap<String, (String, String)>;
 
@@ -10,23 +10,37 @@ enum Direction {
 }
 
 fn parse_line(line: &str) -> (String, String, String) {
-    (line[0..3].to_string(), line[7..10].to_string(), line[12..15].to_string())
+    (
+        line[0..3].to_string(),
+        line[7..10].to_string(),
+        line[12..15].to_string(),
+    )
 }
 
 fn solution1(lines: impl IntoIterator<Item = impl AsRef<str>>) -> u32 {
     let mut lines = lines.into_iter();
-    let dirs: Vec<Direction> = lines.next().unwrap().as_ref().as_bytes().iter().map(|&c| {
-        if c == b'L' {
-            Direction::Left
-        } else {
-            Direction::Right
-        }
-    }).collect();
+    let dirs: Vec<Direction> = lines
+        .next()
+        .unwrap()
+        .as_ref()
+        .as_bytes()
+        .iter()
+        .map(|&c| {
+            if c == b'L' {
+                Direction::Left
+            } else {
+                Direction::Right
+            }
+        })
+        .collect();
 
     let mut map: Map = HashMap::new();
-    lines.skip(1).map(|line| parse_line(line.as_ref())).for_each(|(name, left, right)| {
-        map.insert(name, (left, right));
-    });
+    lines
+        .skip(1)
+        .map(|line| parse_line(line.as_ref()))
+        .for_each(|(name, left, right)| {
+            map.insert(name, (left, right));
+        });
 
     let mut name = &"AAA".to_string();
     let mut i = 0;
@@ -74,7 +88,6 @@ fn build_cycle(mut name: String, map: &Map, dirs: &[Direction]) -> Cycle {
         seen.insert(name.to_string(), idx);
         for dir in dirs {
             zees.push(name.ends_with('Z'));
-            dbg!(&name);
             let (left, right) = map.get(&name).unwrap();
             name = match dir {
                 Direction::Left => left.clone(),
@@ -83,11 +96,9 @@ fn build_cycle(mut name: String, map: &Map, dirs: &[Direction]) -> Cycle {
             idx += 1;
         }
     }
-    dbg!(&name);
 
     let split_idx = seen.get(&name).unwrap();
     let period_len = idx - split_idx;
-    dbg!(&zees);
     let periodic_zees_vec = zees.split_off(*split_idx);
     debug_assert_eq!(period_len, periodic_zees_vec.len());
     let mut periodic_zees = HashSet::new();
@@ -96,11 +107,8 @@ fn build_cycle(mut name: String, map: &Map, dirs: &[Direction]) -> Cycle {
             periodic_zees.insert(i);
         }
     }
-    dbg!(&zees);
-    dbg!(&periodic_zees);
-    if periodic_zees.len() == 0 {
-        panic!()
-    }
+
+    debug_assert_ne!(0, periodic_zees.len());
     Cycle {
         period_len,
         periodic_zees,
@@ -118,11 +126,11 @@ fn merge_cycles(c1: &Cycle, c2: &Cycle) -> Cycle {
     }
 
     let period_len = num::integer::lcm(c1.period_len, c2.period_len);
-    let reps = period_len / c2.period_len;
+    let reps = period_len / c1.period_len;
     for r in 0..reps {
-        for idx in &c2.periodic_zees {
-            if c1.zees(head_len + idx + r * c2.period_len) {
-                periodic_zees.insert(idx + r*c2.period_len);
+        for idx in &c1.periodic_zees {
+            if c2.zees(head_len + idx + r * c1.period_len) {
+                periodic_zees.insert(idx + r * c1.period_len);
             }
         }
     }
@@ -136,51 +144,56 @@ fn merge_cycles(c1: &Cycle, c2: &Cycle) -> Cycle {
 
 fn solution2(lines: impl IntoIterator<Item = impl AsRef<str>>) -> u64 {
     let mut lines = lines.into_iter();
-    let dirs: Vec<Direction> = lines.next().unwrap().as_ref().as_bytes().iter().map(|&c| {
-        if c == b'L' {
-            Direction::Left
-        } else {
-            Direction::Right
-        }
-    }).collect();
+    let dirs: Vec<Direction> = lines
+        .next()
+        .unwrap()
+        .as_ref()
+        .as_bytes()
+        .iter()
+        .map(|&c| {
+            if c == b'L' {
+                Direction::Left
+            } else {
+                Direction::Right
+            }
+        })
+        .collect();
 
     let mut map: HashMap<String, (String, String)> = HashMap::new();
     let mut names: Vec<String> = Vec::new();
 
-    lines.skip(1).map(|line| parse_line(line.as_ref())).for_each(|(name, left, right)| {
-        if name.ends_with('A') {
-            names.push(name.clone());
-        }
-        map.insert(name, (left, right));
-    });
-
-    dbg!(&names);
+    lines
+        .skip(1)
+        .map(|line| parse_line(line.as_ref()))
+        .for_each(|(name, left, right)| {
+            if name.ends_with('A') {
+                names.push(name.clone());
+            }
+            map.insert(name, (left, right));
+        });
 
     // Build loop for each name
-    let final_cycle = names.iter().map(|name| {
-        build_cycle(name.clone(), &map, &dirs)
-    }).reduce(|c1, c2| {
-        println!("reducing cycles with period length {} and {}", c1.period_len, c2.period_len);
-        dbg!(&c1);
-        dbg!(&c2);
-        let c3 = merge_cycles(&c1, &c2);
-        dbg!(&c3);
-        if c3.periodic_zees.len() == 0 {
-            panic!()
-        }
-        c3
-    }).unwrap();
+    let final_cycle = names
+        .iter()
+        .map(|name| build_cycle(name.clone(), &map, &dirs))
+        .reduce(|c1, c2| merge_cycles(&c1, &c2))
+        .unwrap();
 
-    println!("looking for answer in cycle with period length {}", final_cycle.period_len);
-    (final_cycle.head_zees.len() + final_cycle.periodic_zees.iter().min().unwrap()).try_into().unwrap()
+    println!(
+        "looking for answer in cycle with period length {}",
+        final_cycle.period_len
+    );
+    (final_cycle.head_zees.len() + final_cycle.periodic_zees.iter().min().unwrap())
+        .try_into()
+        .unwrap()
 }
 
 fn main() {
     let stdin = io::stdin();
     let lines: Vec<String> = stdin.lock().lines().map(Result::unwrap).collect();
 
-    //let p1 = solution1(&lines);
-    //println!("Part 1: {p1}");
+    let p1 = solution1(&lines);
+    println!("Part 1: {p1}");
     let p2 = solution2(&lines);
     println!("Part 2: {p2}");
 }
